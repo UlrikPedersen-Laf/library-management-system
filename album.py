d9765e6f11b0b3d406e4c83fa899e736d9781142
Ulrik <Ulrikstormpedersen@gmail.com>
2026-05-29 02:26:57 +0200

    Add MediaItem base class and Podcast class - implements polymorphism

--- album.py
+++ album.py
@@ -1,9 +1,26 @@
-class Album:
-    def __init__(self, album_id, title, artist, copies):
-        self.album_id = album_id
-        self.title = title 
-        self.artist = artist
+class MediaItem:
+    def __init__(self, item_id, title, copies):
+        self.item_id = item_id
+        self.title = title
         self.copies = copies
+
     def display_info(self):
-        print(f"ID: {self.album_id} | Title: {self.title} | Artist: {self.artist} | Copies: {self.copies}")
-        
+        print("This method must be implemented by a subclass.")
+
+
+class Album(MediaItem):
+    def __init__(self, album_id, title, artist, copies):
+        super().__init__(album_id, title, copies)
+        self.artist = artist
+
+    def display_info(self):
+        print(f"ID: {self.item_id} | Title: {self.title} | Artist: {self.artist} | Copies: {self.copies}")
+
+
+class Podcast(MediaItem):
+    def __init__(self, podcast_id, title, host, copies):
+        super().__init__(podcast_id, title, copies)
+        self.host = host
+
+    def display_info(self):
+        print(f"ID: {self.item_id} | Title: {self.title} | Host: {self.host} | Copies: {self.copies}")
